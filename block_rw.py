8a0d0fbfc8ff64511cc90efd0a9c0fb825b1d624
elokhar <63730402+elokhar@users.noreply.github.com>
2022-01-17 20:17:57 +0100

    added write_record

--- block_rw.py
+++ block_rw.py
@@ -14,8 +14,8 @@ BLOCK_SIZE = r.RECORD_SIZE*5
 
 records_list = []
 
-def read_block(data_filename):
-    file = open(data_filename, "rb")
+def read_block(data_file):
+    file = data_file
     record_bytes = file.read(r.RECORD_SIZE)
     records_list.append(r.record.from_bytes(record_bytes))
     if(record_bytes==0): #if there is no more data to read, return False
@@ -26,17 +26,28 @@ def read_block(data_filename):
         if(record_bytes==0):
             break
         records_list.append(r.record.from_bytes(record_bytes))
-    file.close()
     return True
 
-def read_record(data_filename):
+def write_block(data_file, record):
+    file = data_file
+    
+
+def read_record(data_file):
     if records_list:
         return records_list.pop(0)
     else:
-        if(read_block(data_filename)):
-            return read_record(data_filename)
+        if(read_block(data_file)):
+            return read_record(data_file)
         else:
             return False
+
+def write_record(data_file, record):
+    if len(records_list) < BLOCK_SIZE:
+        records_list.append(record)
+    else:
+        write_block(data_file, record)
+
+
         
         
 
--- main.py
+++ main.py
@@ -1,24 +1,21 @@
+from ast import For
 import record as r
 import block_rw as b
 
 
 firstRecordsList = r.createRandomRecords(15)
 
-
-file = open("file.dat", "wb")
+file = open("file.dat", "w+b")
 
 for record in firstRecordsList:
     file.write(bytes(record))
-file.close()
+file.flush()
+file.seek(0)
 
 firstRecordsList.clear()
 
-print(b.read_record("file.dat"))
-print(b.read_record("file.dat"))
-print(b.read_record("file.dat"))
-print(b.read_record("file.dat"))
-print(b.read_record("file.dat"))
-
-print(b.read_record("file.dat"))
+for i in range(15):
+    print(b.read_record(file))
+file.close()
 
 
