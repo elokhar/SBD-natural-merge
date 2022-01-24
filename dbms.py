446f147dd905340ab5bd3b3609b85e408d09e920
Piotr Wojciechowski <wojpiter2@interia.pl>
2022-01-24 17:15:06 +0100

    fixed merge sort algorythm

--- dbms.py
+++ dbms.py
@@ -1,3 +1,4 @@
+from importlib.resources import read_binary
 from record import record as record
 from block_rw import read_record, start_reading_from_beginning, write_record
 from printing import print_file
@@ -14,8 +15,7 @@ def sort(file, show_phases=False):
     start_reading_from_beginning(file)
     tape1 = open("tape1.dat", "w+b")
     tape2 = open("tape2.dat", "w+b")
-    while(file_sorted==False):
-        
+    while(file_sorted==False):        
 
         distribute(file, tape1, tape2)
 
@@ -37,14 +37,7 @@ def sort(file, show_phases=False):
                 print_file(file)        
                 start_reading_from_beginning(tape1)
                 start_reading_from_beginning(tape2)
-                start_reading_from_beginning(file)
-            
-
-            
-        
-            
-
-        
+                start_reading_from_beginning(file)    
 
     print()    
     print("File sorted")
@@ -76,7 +69,7 @@ def distribute(src_tape, dest_tape_1, dest_tape_2):
         while(next_record!=None):            
             curr_key = curr_record.getKey()
             next_key = next_record.getKey()
-            run_end = check_run_end(curr_key, next_key)
+            run_end = check_run_end(curr_record, next_record)
             if(run_end):
                 #alternate destination tape
                 dest_tape_number = (dest_tape_number+1)%2
@@ -88,33 +81,52 @@ def distribute(src_tape, dest_tape_1, dest_tape_2):
 
 def merge(src_tape_1, src_tape_2, dest_tape):
     
-    record1 = read_record(src_tape_1)
-    record2 = read_record(src_tape_2)
-    if(record1==None or record2==None):
+    tape1_curr = read_record(src_tape_1)
+    tape1_next = read_record(src_tape_1)
+    tape2_curr = read_record(src_tape_2)
+    tape2_next = read_record(src_tape_2)
+    tape1_run_end = False
+    tape2_run_end = False
+    tape_read = 0
+    
+    if(tape1_curr==None or tape2_curr==None):
         file_sorted = True
     else:
         file_sorted = False
         dest_tape.truncate(0)
         dest_tape.seek(0)
-        while(record1!=None or record2!=None):
-            if(record2==None):
-                write_record(dest_tape, record1)
-                record1 = read_record(src_tape_1)
-            elif(record1==None):
-                write_record(dest_tape, record2)
-                record2 = read_record(src_tape_2)
-            elif(record1.getKey()<record2.getKey()):
-                write_record(dest_tape, record1)
-                record1 = read_record(src_tape_1)
+        while(tape1_curr!=None or tape2_curr!=None):
+            if(tape1_run_end and tape2_run_end):
+                tape1_run_end = False
+                tape2_run_end = False
+            if(tape2_curr==None or tape2_run_end):
+                tape_read = 1
+            elif(tape1_curr==None or tape1_run_end):
+                tape_read = 2
+            elif(tape1_curr.getKey()<tape2_curr.getKey()):          
+                tape_read = 1
+            else:            
+                tape_read = 2
+            if(tape_read==1):
+                tape1_run_end = check_run_end(tape1_curr, tape1_next)
+                write_record(dest_tape, tape1_curr)
+                tape1_curr = tape1_next
+                tape1_next = read_record(src_tape_1)
             else:
-                write_record(dest_tape, record2)
-                record2 = read_record(src_tape_2)
+                tape2_run_end = check_run_end(tape2_curr, tape2_next)
+                write_record(dest_tape, tape2_curr)
+                tape2_curr = tape2_next
+                tape2_next = read_record(src_tape_2)
+
+
     return file_sorted
 
     
 
-def check_run_end(curr_key, next_key):
-    if curr_key <= next_key:
+def check_run_end(curr_record, next_record):
+    if(next_record==None):
+        return True
+    if curr_record.getKey() <= next_record.getKey():
         return False
     else:
         return True
